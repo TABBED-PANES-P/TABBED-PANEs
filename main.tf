fa9e62341a4db15e4b880d05fd189b9fb29d0d96
Shire <mohamoudabdiweli80@gmail.com>
2025-04-02 20:03:37 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,40 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources in"
   type        = string
-  default     = "us-east-1"  # You can change this to any region you prefer
+}
+
+# Declare the s3_bucket_name variable
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+# Declare RDS instance details variables
+variable "db_instance_name" {
+  description = "The name of the RDS instance"
+  type        = string
+}
+
+variable "db_username" {
+  description = "The username for the RDS instance"
+  type        = string
+}
+
+variable "db_password" {
+  description = "The password for the RDS instance"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_name" {
+  description = "The database name"
+  type        = string
+}
+
+# Declare allowed_ip_ranges for security group
+variable "allowed_ip_ranges" {
+  description = "Allowed IP ranges for the security group"
+  type        = list(string)
 }
 
 # Configure the AWS provider
@@ -10,9 +43,9 @@ provider "aws" {
   region = var.aws_region  # Referencing the aws_region variable
 }
 
-# Create a unique S3 Bucket with a sanitized name
+# Create the S3 Bucket with the provided name
 resource "aws_s3_bucket" "example" {
-  bucket = "my-terraform-bucket-2025-04-02T160044Z"  # Updated bucket name with valid characters
+  bucket = var.s3_bucket_name  # Using the variable from terraform.tfvars
 
   tags = {
     Name        = "MyBucket"
@@ -28,7 +61,7 @@ resource "aws_security_group" "rds_sg" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # For wide access, but not recommended for production
+    cidr_blocks = var.allowed_ip_ranges  # Using the allowed_ip_ranges variable
   }
   
   egress {
@@ -45,14 +78,14 @@ resource "aws_security_group" "rds_sg" {
 
 # Create an RDS instance and associate the security group
 resource "aws_db_instance" "example" {
-  identifier          = "my-db-instance"
+  identifier          = var.db_instance_name  # Using the db_instance_name variable
   engine              = "mysql"
   instance_class      = "db.t3.micro"
-  allocated_storage   = 20  # Fixed the duplicate issue
+  allocated_storage   = 20
   storage_type        = "gp2"
-  username            = "admin"
-  password            = "admin1234"  # Change this to a more secure password
-  db_name             = "mydb"
+  username            = var.db_username  # Using the db_username variable
+  password            = var.db_password  # Using the db_password variable
+  db_name             = var.db_name      # Using the db_name variable
   publicly_accessible = true
   
   vpc_security_group_ids = [aws_security_group.rds_sg.id]  # Associating security group
@@ -66,5 +99,11 @@ resource "aws_db_instance" "example" {
     Environment = "Dev"
   }
 
+  auto_minor_version_upgrade = true
+
+  # Enable RDS instance encryption for data-at-rest (recommended for production)
+  storage_encrypted = true
+
+  # If you want to enable automatic minor version upgrade
   auto_minor_version_upgrade = true
 }
