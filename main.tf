1fd361eea7ef9a34f6ce2ce3a564ddbfcd92adbb
Shire <mohamoudabdiweli80@gmail.com>
2025-04-02 17:56:03 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,17 @@
+stage('Terraform Init') {
+    steps {
+        sh 'terraform init'
+    }
+}
+
+stage('Terraform Plan') {
+    steps {
+        sh 'terraform plan -out=tfplan'
+    }
+}
+
+stage('Terraform Apply') {
+    steps {
+        sh 'terraform apply -auto-approve tfplan'
+    }
+}
