5b6d4af1bf72013157f9eadf7e8dd8b1e9e82539
Shire <mohamoudabdiweli80@gmail.com>
2025-04-03 05:02:57 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,35 @@
-variable "instance_type" {
-  description = "The type of EC2 instance for the RDS instance"
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  default     = "prod"
+}
+
+variable "mysql_version" {
+  description = "MySQL version"
+  default     = "8.0.33"
+}
+
+variable "instance_class" {
+  description = "RDS instance class"
   default     = "db.t3.micro"
 }
 
+variable "allocated_storage" {
+  description = "Storage size in GB"
+  default     = 20
+}
+
+variable "db_name" {
+  description = "Database name"
+  default     = "applicationdb"
+}
+
 variable "db_username" {
-  description = "Database administrator username"
+  description = "Database admin username"
   default     = "admin"
 }
 
@@ -13,10 +38,17 @@ variable "db_password" {
   sensitive   = true
 }
 
-variable "s3_bucket_name" {
-  description = "The S3 bucket name"
+variable "allowed_cidr" {
+  description = "Allowed CIDR block for MySQL access"
+  default     = "10.0.0.0/16"
 }
 
-variable "aws_ami_id" {
-  description = "The AMI ID"
+variable "skip_final_snapshot" {
+  description = "Skip final snapshot when destroying"
+  default     = true
+}
+
+variable "publicly_accessible" {
+  description = "Make RDS publicly accessible"
+  default     = false
 }
