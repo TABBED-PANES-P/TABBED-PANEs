20305bba22354099fe8c810f3bef5ee635a51538
Shire <mohamoudabdiweli80@gmail.com>
2025-04-03 03:28:51 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,32 +1,22 @@
-variable "region" {
-  description = "The AWS region to deploy resources in"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "instance_type" {
-  description = "The type of EC2 instance"
-  type        = string
+  description = "The type of EC2 instance for the RDS instance"
+  default     = "db.t3.micro"
 }
 
 variable "db_username" {
   description = "Database administrator username"
-  type        = string
-  sensitive   = true
+  default     = "admin"
 }
 
 variable "db_password" {
-  description = "Database administrator password"
-  type        = string
+  description = "Database password"
   sensitive   = true
 }
 
 variable "s3_bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
+  description = "The S3 bucket name"
 }
 
 variable "aws_ami_id" {
-  description = "The AMI ID to use for the EC2 instance"
-  type        = string
+  description = "The AMI ID"
 }
